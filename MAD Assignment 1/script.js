45af4a2b5a2b64dd996093a7c566e3ec18833d85
reisou12 <33390356+reisou12@users.noreply.github.com>
2017-11-06 11:48:57 +0800

    Add files via upload

---
+++ MAD Assignment 1/script.js
@@ -0,0 +1,7 @@
+var Catergory = prompt("Please enter the catergory of the expense","Transportation");
+var Amount = parseFloat(prompt("Please enter the amount of expense in dollars","10"));
+var Payee = prompt("Please enter the name of the payee","MacDonald");
+var DateOfExpense = prompt("Please enter the date of the expense","Transportation");
+
+
+document.write(Catergory+Amount+Payee+DateOfExpense);
